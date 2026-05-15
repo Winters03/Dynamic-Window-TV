d3871f350d2a5ba0951acbca4e7e15afdf270a75
SamuelPardoe <144715785+SamuelPardoe@users.noreply.github.com>
2026-05-15 13:16:53 -0400

    Auto-commit: 2026-05-15 13:16:53

--- pushChanges.sh
+++ pushChanges.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-# Get the commit message from the first argument
-# Use current date/time if no message is provided
-MESSAGE=${1:-"Auto-commit: $(date +'%Y-%m-%d %H:%M:%S')"}
+rm -rf build
 
-# Stage all changes
+MESSAGE=${1:-"Auto-commit: $(date +'%Y-%m-%d %H:%M:%S')"}
 git add .
 
-# Commit with the message
 git commit -m "$MESSAGE"
 
-# Push to the current branch
-# Note: This assumes your upstream is already set
 git push
